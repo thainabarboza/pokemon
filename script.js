28ddb2f3ec4a0faf92619c86e376885bc9e4a6d8
carolcallau <caroubmr@gmail.com>
2026-04-14 17:53:23 +0100

    adiciona o scrol infinito na página allPokemons.html

--- script.js
+++ script.js
@@ -1,49 +1,67 @@
 let containerCard = document.querySelector(".container-card");
+let loading = false;
+let offset = 0;
 
 async function getPokemons() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=24&offset=0");
-    const data = await response.json();
+    if (!loading) {
+        loading = true;
 
-    for (let i = 0; i < data.results.length; i++) {
-        const infoPokemons = await fetch(`${data.results[i].url}`);
-        const pokemon = await infoPokemons.json();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=24&offset=${offset}`);
+        const data = await response.json();
 
-        containerCard.innerHTML += `
-            <div class="flip-card">
-                <div class="flip-inner">
-                    <div class="card-front">
-                        <div class="topo-card">
-                            <p>00${pokemon.id}</p>
-                            <button id="${pokemon.name}" class="btn-favorito" onclick="handleFavourite('${pokemon.name}', this)">
-                                <img src="svg/material-symbols_favorite-outline-rounded.svg">
-                            </button> 
+        let pokemons = []
+
+        for (let i = 0; i < data.results.length; i++) {
+            const infoPokemons = await fetch(`${data.results[i].url}`);
+            pokemons.push(await infoPokemons.json());
+        }
+
+        for (let i = 0; i < pokemons.length; i++) {
+            containerCard.innerHTML += `
+                <div class="flip-card">
+                    <div class="flip-inner">
+                        <div class="card-front">
+                            <div class="topo-card">
+                                <p>00${pokemons[i].id}</p>
+                                <button id="${pokemons[i].name}" class="btn-favorito" onclick="handleFavourite('${pokemons[i].name}', this)">
+                                    <img src="svg/material-symbols_favorite-outline-rounded.svg">
+                                </button> 
+                            </div>
+                            <button class="img-card">
+                                <a href="./pagePokemon.html">
+                                    <img src="${pokemons[i].sprites.other["official-artwork"].front_default}" alt="">
+                                </a>
+                            </button>
+                            <h3>${pokemons[i].name}</h3>
+                            <div class="elements">
+                                ${pokemons[i].types.map(elemento => `
+                                    <div class="tag-element ${elemento.type.name}">
+                                        <span>${elemento.type.name}</span>
+                                    </div>
+                                `).join('')}
+                            </div>
+                        </div> 
+
+                        <div class="card-back">
+                            <img class="img-back" src="img/Rectangle 5.png" alt="">
                         </div>
-                        <button class="img-card">
-                            <a href="./pagePokemon.html">
-                                <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="">
-                            </a>
-                        </button>
-                        <h3>${pokemon.name}</h3>
-                        <div class="elements">
-                            ${pokemon.types.map(elemento => `
-                                <div class="tag-element ${elemento.type.name}">
-                                    <span>${elemento.type.name}</span>
-                                </div>
-                            `).join('')}
-                        </div>
-                    </div> 
-
-                    <div class="card-back">
-                        <img class="img-back" src="img/Rectangle 5.png" alt="">
                     </div>
-                </div>
-            </div>`;
+                </div>`;
 
-        checkFavourite(pokemon.name);
+            checkFavourite(pokemons[i].name);
+        }
+        loading = false;
+        offset += 24
     }
-}
+}  
 
 getPokemons();
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY + window.innerHeight + 5 > document.body.scrollHeight - 500) {
+        getPokemons();        
+    }
+})
 
 function handleFavourite(nome, botao) {
     const flipCard = botao.closest(".flip-card");
